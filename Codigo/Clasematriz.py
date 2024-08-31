b4e7463ce4efa035ea6ea8d7dcc2781ceb3da25a
Romina-SIS112 <romina.torrez@ucb.edu.bo>
2024-08-31 17:30:12 -0400

    Práctica en clase

---
+++ Codigo/Clasematriz.py
@@ -0,0 +1,20 @@
+lista = [1, 2, 3, 4, 5]
+#
+print(lista)
+print(lista[2])
+matriz = [ 
+    [ 1, 2, 3 ]
+    [ 4, 5, 6]
+    [ 7, 8, 9]
+]
+print(matriz[1][2])
+listastring = ["a", "b", "c", "d"]
+print(type(listastring[3]))
+listastring =["a",2.2,"c",10,["z","x","9"]]
+print(type(listastring[4][2]))
+
+
+
+
+
+
